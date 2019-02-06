f9da71b7e1427e9751cc47a1af3537bfa36125d3
Andriy <Kyrylchuk>
2019-02-06 16:56:47 +0200

    Initial commit;

---
+++ CustomerTransaction/Controllers/CustomerController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CustomerTransaction.Controllers
+{
+    [Route("api/customers")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        /// <summary>
+        /// Get Customers
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
