42e1c992cfcce4708059c2fce9f7286869670c23
Andriy <Kyrylchuk>
2019-02-08 16:00:19 +0200

    Added Date formatter;

---
+++ CustomerTransaction/CustomDateTimeFormat.cs
@@ -0,0 +1,12 @@
+﻿using Newtonsoft.Json.Converters;
+
+namespace CustomerTransaction
+{
+    public class CustomDateTimeFormater: IsoDateTimeConverter
+    {
+        public CustomDateTimeFormater()
+        {
+            base.DateTimeFormat = "dd/MM/yyyy HH:MM";
+        }
+    }
+}
--- CustomerTransaction/Models/TransactionDto.cs
+++ CustomerTransaction/Models/TransactionDto.cs
@@ -1,12 +1,14 @@
 ﻿using System;
+using Newtonsoft.Json;
 
 namespace CustomerTransaction.Models
 {
     public class TransactionDto
     {
         public Int32 Id { get; set; }
+        [JsonConverter(typeof(CustomDateTimeFormater))]
         public DateTime Date { get; set; }
-        public Decimal Amount { get; set; }
+        public String Amount { get; set; }
         public String Currency { get; set; }
         public String Status { get; set; }
     }
